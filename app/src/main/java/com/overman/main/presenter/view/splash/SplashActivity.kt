52b675105ffc54b7683310cca02032dddbbb992d
overman623 <overman623@naver.com>
2022-06-24 21:40:10 +0900

    feat(splash) : splash 클래스 추가

---
+++ app/src/main/java/com/overman/main/presenter/view/splash/SplashActivity.kt
@@ -0,0 +1,9 @@
+package com.overman.main.presenter.view.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SplashActivity : AppCompatActivity() {
+}
+
