a2e596081bf297153526fe0e9c3820059b03fc6e
overman623 <overman623@naver.com>
2022-07-03 15:28:40 +0900

    fix(imageSelect) : DiffUtil 코드 추가

--- app/src/main/java/com/overman/main/presenter/view/imageselect/ImageAdapter.kt
+++ app/src/main/java/com/overman/main/presenter/view/imageselect/ImageAdapter.kt
@@ -2,11 +2,19 @@ package com.overman.main.presenter.view.imageselect
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.overman.main.databinding.ItemImageBinding
 import com.overman.main.domain.model.image.Image
 
 class ImageAdapter(private val imageList: MutableList<Image>): RecyclerView.Adapter<ImageViewHolder>() {
+
+    fun setImageList(updatedImageList: List<Image>) {
+        val diffResult = DiffUtil.calculateDiff(ImageDiffUtilCallback(imageList, updatedImageList))
+        imageList.clear()
+        imageList.addAll(updatedImageList)
+        diffResult.dispatchUpdatesTo(this)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
         val binding = ItemImageBinding.inflate(LayoutInflater.from(parent.context))
